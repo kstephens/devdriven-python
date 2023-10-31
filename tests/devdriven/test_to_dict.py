2e67757284445155cc98fa2b10619952b5aff72e
Kurt Stephens <git@kurtstephens.com>
2023-10-31 16:06:58 -0500

    Massive reindent and delint.

--- tests/devdriven/test_to_dict.py
+++ tests/devdriven/test_to_dict.py
@@ -5,65 +5,66 @@ import pytest
 import devdriven.to_dict
 
 def test_walk():
-    fut = devdriven.to_dict.to_dict
-    assert fut(None) is None
-    assert fut(123) == 123
-    assert fut(123.45) == 123.45
-    assert fut('abc') == 'abc'
-    assert fut(b'xyz') == '<BYTES[3]:xyz>'
-    assert fut(b'\xffqwerty') == '<BYTES[7]>'
-    assert fut('🤷'.encode('utf-8')) == '<BYTES[4]:🤷>'
-    assert fut([123, b'xyz']) == [123, '<BYTES[3]:xyz>']
-    assert (fut({'a': b'xyz', 'b': 123, 'c': b'wsad'}) ==
-            {'a': '<BYTES[3]:xyz>', 'b': 123, 'c': '<BYTES[4]:wsad>'})
-    assert fut(re.compile(r'^rx', re.IGNORECASE)) == "re.compile('^rx', re.IGNORECASE)"
-    assert fut(datetime.fromordinal(1234567)) == "3381-02-16T00:00:00.000000"
-    assert fut(Exception("E")) == {'class': 'Exception', 'message': 'E'}
-    other = devdriven.to_dict.ToDict()
-    assert fut(other) == other
-    fut = devdriven.to_dict.ToDict()
-    assert fut(None) is None
+  fut = devdriven.to_dict.to_dict
+  assert fut(None) is None
+  assert fut(123) == 123
+  assert fut(123.45) == 123.45
+  assert fut('abc') == 'abc'
+  assert fut(b'xyz') == '<BYTES[3]:xyz>'
+  assert fut(b'\xffqwerty') == '<BYTES[7]>'
+  assert fut('🤷'.encode('utf-8')) == '<BYTES[4]:🤷>'
+  assert fut([123, b'xyz']) == [123, '<BYTES[3]:xyz>']
+  assert fut(
+    {'a': b'xyz', 'b': 123, 'c': b'wsad'}) == \
+    {'a': '<BYTES[3]:xyz>', 'b': 123, 'c': '<BYTES[4]:wsad>'}
+  assert fut(re.compile(r'^rx', re.IGNORECASE)) == "re.compile('^rx', re.IGNORECASE)"
+  assert fut(datetime.fromordinal(1234567)) == "3381-02-16T00:00:00.000000"
+  assert fut(Exception("E")) == {'class': 'Exception', 'message': 'E'}
+  other = devdriven.to_dict.ToDict()
+  assert fut(other) == other
+  fut = devdriven.to_dict.ToDict()
+  assert fut(None) is None
 
 def test_dump_json():
-    fut = devdriven.to_dict.dump_json
-    result = fut({'a': b'xyz', 'b': 123, 'c': b'wsad'}, 2)
-    assert result == '{\n  "a": "<BYTES[3]:xyz>",\n  "b": 123,\n  "c": "<BYTES[4]:wsad>"\n}'
+  fut = devdriven.to_dict.dump_json
+  result = fut({'a': b'xyz', 'b': 123, 'c': b'wsad'}, 2)
+  assert result == '{\n  "a": "<BYTES[3]:xyz>",\n  "b": 123,\n  "c": "<BYTES[4]:wsad>"\n}'
 
 def test_walk_oserror():
-    fut = devdriven.to_dict.to_dict
-    with pytest.raises(OSError) as exc_info:
-        open_file('Does-Not-Exist')
-    assert fut(exc_info.value) == {
-        'class': 'FileNotFoundError',
-        'message': "[Errno 2] No such file or directory: 'Does-Not-Exist'",
-        'errno': 2,
-        'strerror': 'No such file or directory',
-        'filename': 'Does-Not-Exist',
-        'filename2': None
-    }
+  fut = devdriven.to_dict.to_dict
+  with pytest.raises(OSError) as exc_info:
+    open_file('Does-Not-Exist')
+  assert fut(exc_info.value) == {
+    'class': 'FileNotFoundError',
+    'message': "[Errno 2] No such file or directory: 'Does-Not-Exist'",
+    'errno': 2,
+    'strerror': 'No such file or directory',
+    'filename': 'Does-Not-Exist',
+    'filename2': None
+  }
 
 def open_file(name):
-    with open(name, 'rb') as file:
-        return file.read()
+  with open(name, 'rb') as file:
+    return file.read()
 
 def test_walk_subprocess():
-    fut = devdriven.to_dict.to_dict
-    result = subprocess.run(['/bin/echo', 'OK'], check=True, capture_output=True)
-    assert fut(result) == {
-        'args': ['/bin/echo', 'OK'],
-        'returncode': 0,
-        'stdout': '<BYTES[3]:OK\n>',
-        'stderr': '<BYTES[0]>'
-    }
+  fut = devdriven.to_dict.to_dict
+  result = subprocess.run(['/bin/echo', 'OK'], check=True, capture_output=True)
+  assert fut(result) == {
+    'args': ['/bin/echo', 'OK'],
+    'returncode': 0,
+    'stdout': '<BYTES[3]:OK\n>',
+    'stderr': '<BYTES[0]>'
+  }
 
 def test_walk_subprocess_error():
-    fut = devdriven.to_dict.to_dict
-    with pytest.raises(subprocess.CalledProcessError) as exc_info:
-        subprocess.run(['/bin/false'], check=True)
-    assert fut(exc_info.value) == {
-        'class': 'CalledProcessError',
-        'message': "Command '['/bin/false']' returned non-zero exit status 1.",
-        'returncode': 1,
-        'stdout': None,
-        'stderr': None,
-    }
+  fut = devdriven.to_dict.to_dict
+  with pytest.raises(subprocess.CalledProcessError) as exc_info:
+    subprocess.run(['/bin/false'], check=True)
+  assert fut(exc_info.value) == {
+    'class': 'CalledProcessError',
+    'message': "Command '['/bin/false']' returned non-zero exit status 1.",
+    'returncode': 1,
+    'stdout': None,
+    'stderr': None,
+  }
--- tests/devdriven/test_util.py
+++ tests/devdriven/test_util.py
@@ -2,160 +2,160 @@ import subprocess
 from devdriven import util
 
 def test_maybe_decode_bytes():
-    assert util.maybe_decode_bytes(b'A') == 'A'
-    assert util.maybe_decode_bytes(b'\xff') is None
-    assert util.maybe_decode_bytes(None) is None
+  assert util.maybe_decode_bytes(b'A') == 'A'
+  assert util.maybe_decode_bytes(b'\xff') is None
+  assert util.maybe_decode_bytes(None) is None
 
 def test_exec_command():
-    result = util.exec_command(['echo', 'OK'])
-    assert result.returncode == 0
-    assert result.stdout is None
-    assert result.stderr is None
+  result = util.exec_command(['echo', 'OK'])
+  assert result.returncode == 0
+  assert result.stdout is None
+  assert result.stderr is None
 
-    result = util.exec_command(['echo', 'OK'],
-                               capture_output=True)
-    assert result.returncode == 0
-    assert result.stdout == b'OK\n'
-    assert result.stderr == b''
+  result = util.exec_command(['echo', 'OK'],
+                             capture_output=True)
+  assert result.returncode == 0
+  assert result.stdout == b'OK\n'
+  assert result.stderr == b''
 
-    result = util.exec_command(['echo ERROR >&2'],
-                               capture_output=True, shell=True)
-    assert result.returncode == 0
-    assert result.stdout == b''
-    assert result.stderr == b'ERROR\n'
+  result = util.exec_command(['echo ERROR >&2'],
+                             capture_output=True, shell=True)
+  assert result.returncode == 0
+  assert result.stdout == b''
+  assert result.stderr == b'ERROR\n'
 
-    ex = None
-    try:
-        util.exec_command(['false'], check=True)
-    except subprocess.CalledProcessError as err:
-        ex = err
-    assert ex is not None
+  ex = None
+  try:
+    util.exec_command(['false'], check=True)
+  except subprocess.CalledProcessError as err:
+    ex = err
+  assert ex is not None
 
 def test_read_file():
-    assert len(util.read_file('Makefile')) > 0
-    assert len(util.read_file('Makefile', default=b'DEFAULT')) > 10
-    assert len(util.read_file('UNKNOWN-FILE', default=b'DEFAULT')) == 7
+  assert len(util.read_file('Makefile')) > 0
+  assert len(util.read_file('Makefile', default=b'DEFAULT')) > 10
+  assert len(util.read_file('UNKNOWN-FILE', default=b'DEFAULT')) == 7
 
 def test_file_size():
-    assert util.file_size('Makefile') > 99
-    assert util.file_size('Makefile', default=7) > 7
-    assert util.file_size('UNKNOWN-FILE') is None
-    assert util.file_size('UNKNOWN-FILE', default=7) == 7
+  assert util.file_size('Makefile') > 99
+  assert util.file_size('Makefile', default=7) > 7
+  assert util.file_size('UNKNOWN-FILE') is None
+  assert util.file_size('UNKNOWN-FILE', default=7) == 7
 
 def test_partition():
-    assert util.partition(['a', 1, 'b', 2], lambda e: isinstance(e, str)) == (['a', 'b'], [1, 2])
+  assert util.partition(['a', 1, 'b', 2], lambda e: isinstance(e, str)) == (['a', 'b'], [1, 2])
 
 def test_frequency():
-    assert util.frequency(['a', 1, 'a', 2]) == {'a': 2, 1: 1, 2: 1}
+  assert util.frequency(['a', 1, 'a', 2]) == {'a': 2, 1: 1, 2: 1}
 
 def test_uniq_by():
-    assert util.uniq_by([['a', 1], ['b', 2], ['a', 2]], key=lambda e: e[0]) == [['a', 1], ['b', 2]]
+  assert util.uniq_by([['a', 1], ['b', 2], ['a', 2]], key=lambda e: e[0]) == [['a', 1], ['b', 2]]
 
 def test_parse_commands():
-    assert not util.parse_commands([])
-    assert util.parse_commands(['cmd1']) == \
-        [['cmd1']]
-    assert util.parse_commands(['cmd1', 'a']) == \
-        [['cmd1', 'a']]
-    assert util.parse_commands(['cmd1', 'a', 'b', ',', 'cmd2', 'c']) == \
-        [['cmd1', 'a', 'b'], ['cmd2', 'c']]
+  assert not util.parse_commands([])
+  assert util.parse_commands(['cmd1']) == \
+    [['cmd1']]
+  assert util.parse_commands(['cmd1', 'a']) == \
+    [['cmd1', 'a']]
+  assert util.parse_commands(['cmd1', 'a', 'b', ',', 'cmd2', 'c']) == \
+    [['cmd1', 'a', 'b'], ['cmd2', 'c']]
 
 def test_file_md5():
-    assert util.file_md5('/dev/null') == 'd41d8cd98f00b204e9800998ecf8427e'
-    assert util.file_md5('Does-Not-Exist') is None
+  assert util.file_md5('/dev/null') == 'd41d8cd98f00b204e9800998ecf8427e'
+  assert util.file_md5('Does-Not-Exist') is None
 
 def test_file_nlines():
-    assert util.file_nlines('tests/data/expected.txt', default=2) == 11
-    assert util.file_nlines('/dev/null', default=3) == 0
-    assert util.file_nlines('Does-Not-Exist', default=5) == 5
-    assert util.file_nlines('Does-Not-Exist') is None
+  assert util.file_nlines('tests/data/expected.txt', default=2) == 11
+  assert util.file_nlines('/dev/null', default=3) == 0
+  assert util.file_nlines('Does-Not-Exist', default=5) == 5
+  assert util.file_nlines('Does-Not-Exist') is None
 
 def test_elapsed_ms():
-    result, dt_ms = util.elapsed_ms(lambda x, y: 1 + x + y, 2, y=3)
-    assert result == 6
-    assert dt_ms > 0
+  result, dt_ms = util.elapsed_ms(lambda x, y: 1 + x + y, 2, y=3)
+  assert result == 6
+  assert dt_ms > 0
 
 def test_diff_files():
-    assert util.file_nlines('tests/data/expected.txt') == 11
-    assert util.file_nlines('tests/data/actual.txt') == 10
+  assert util.file_nlines('tests/data/expected.txt') == 11
+  assert util.file_nlines('tests/data/actual.txt') == 10
 
-    result = util.diff_files('tests/data/expected.txt', 'tests/data/actual.txt')
-    expected = {
-        "correct": False,
-        "expected": 11,
-        "actual": 10,
-        "old": 4,
-        "new": 3,
-        "differences": 7,
-        "correct_ratio": 0.6666666666666666,
-        "correct_percent": 66.67,
-        "exit_code": 1
-    }
-    assert result == expected
+  result = util.diff_files('tests/data/expected.txt', 'tests/data/actual.txt')
+  expected = {
+    "correct": False,
+    "expected": 11,
+    "actual": 10,
+    "old": 4,
+    "new": 3,
+    "differences": 7,
+    "correct_ratio": 0.6666666666666666,
+    "correct_percent": 66.67,
+    "exit_code": 1
+  }
+  assert result == expected
 
-    result = util.diff_files('tests/data/expected.txt', 'tests/data/expected.txt')
-    expected = {
-        "correct": True,
-        "expected": 11,
-        "actual": 11,
-        "old": 0,
-        "new": 0,
-        "differences": 0,
-        "correct_ratio": 1.0,
-        "correct_percent": 100.0,
-        "exit_code": 0
-    }
-    assert result == expected
+  result = util.diff_files('tests/data/expected.txt', 'tests/data/expected.txt')
+  expected = {
+    "correct": True,
+    "expected": 11,
+    "actual": 11,
+    "old": 0,
+    "new": 0,
+    "differences": 0,
+    "correct_ratio": 1.0,
+    "correct_percent": 100.0,
+    "exit_code": 0
+  }
+  assert result == expected
 
-    result = util.diff_files('tests/data/expected.txt', 'Does-Not-Exist.txt')
-    expected = {
-        "correct": False,
-        "expected": 11,
-        "actual": None,
-        "old": None,
-        "new": None,
-        "differences": None,
-        "correct_ratio": 0.0,
-        "correct_percent": 0.0,
-        "exit_code": 2
-    }
-    assert result == expected
+  result = util.diff_files('tests/data/expected.txt', 'Does-Not-Exist.txt')
+  expected = {
+    "correct": False,
+    "expected": 11,
+    "actual": None,
+    "old": None,
+    "new": None,
+    "differences": None,
+    "correct_ratio": 0.0,
+    "correct_percent": 0.0,
+    "exit_code": 2
+  }
+  assert result == expected
 
 def test_diff_files_stats():
-    assert diff_files_stats(0, 0, 0, 0, 0) == \
-        {'correct': True, 'expected': 0, 'actual': 0,
-         'old': 0, 'new': 0, 'differences': 0,
-         'correct_ratio': 1.0, 'correct_percent': 100.0, 'exit_code': 0}
-    assert diff_files_stats(10, 10, 0, 0, 0) == \
-        {'correct': True, 'expected': 10, 'actual': 10,
-         'old': 0, 'new': 0, 'differences': 0,
-         'correct_ratio': 1.0, 'correct_percent': 100.0, 'exit_code': 0}
-    assert diff_files_stats(10, 9, 1, 0, 1) == \
-        {'correct': False, 'expected': 10, 'actual': 9,
-         'old': 1, 'new': 0, 'differences': 1,
-         'correct_ratio': 0.9473684210526315, 'correct_percent': 94.74, 'exit_code': 1}
-    assert diff_files_stats(9, 10, 0, 1, 1) == \
-        {'correct': False, 'expected': 9, 'actual': 10,
-         'old': 0, 'new': 1, 'differences': 1,
-         'correct_ratio': 0.9473684210526315, 'correct_percent': 94.74, 'exit_code': 1}
-    assert diff_files_stats(10, 9, 1, 0, 1) == \
-        {'correct': False, 'expected': 10, 'actual': 9,
-         'old': 1, 'new': 0, 'differences': 1,
-         'correct_ratio': 0.9473684210526315, 'correct_percent': 94.74, 'exit_code': 1}
-    assert diff_files_stats(10, 11, 0, 1, 1) == \
-        {'correct': False, 'expected': 10, 'actual': 11,
-         'old': 0, 'new': 1, 'differences': 1,
-         'correct_ratio': 0.9523809523809523, 'correct_percent': 95.24, 'exit_code': 1}
-    assert diff_files_stats(10, 10, 1, 1, 1) == \
-        {'correct': False, 'expected': 10, 'actual': 10,
-         'old': 1, 'new': 1, 'differences': 2,
-         'correct_ratio': 0.9, 'correct_percent': 90.0, 'exit_code': 1}
-    assert diff_files_stats(0, 10, 0, 10, 0) == \
-        {'correct': False, 'expected': 0, 'actual': 10,
-         'old': 0, 'new': 10, 'differences': 10,
-         'correct_ratio': 0.0, 'correct_percent': 0.0, 'exit_code': 0}
+  assert diff_files_stats(0, 0, 0, 0, 0) == \
+    {'correct': True, 'expected': 0, 'actual': 0,
+     'old': 0, 'new': 0, 'differences': 0,
+     'correct_ratio': 1.0, 'correct_percent': 100.0, 'exit_code': 0}
+  assert diff_files_stats(10, 10, 0, 0, 0) == \
+    {'correct': True, 'expected': 10, 'actual': 10,
+     'old': 0, 'new': 0, 'differences': 0,
+     'correct_ratio': 1.0, 'correct_percent': 100.0, 'exit_code': 0}
+  assert diff_files_stats(10, 9, 1, 0, 1) == \
+    {'correct': False, 'expected': 10, 'actual': 9,
+     'old': 1, 'new': 0, 'differences': 1,
+     'correct_ratio': 0.9473684210526315, 'correct_percent': 94.74, 'exit_code': 1}
+  assert diff_files_stats(9, 10, 0, 1, 1) == \
+    {'correct': False, 'expected': 9, 'actual': 10,
+     'old': 0, 'new': 1, 'differences': 1,
+     'correct_ratio': 0.9473684210526315, 'correct_percent': 94.74, 'exit_code': 1}
+  assert diff_files_stats(10, 9, 1, 0, 1) == \
+    {'correct': False, 'expected': 10, 'actual': 9,
+     'old': 1, 'new': 0, 'differences': 1,
+     'correct_ratio': 0.9473684210526315, 'correct_percent': 94.74, 'exit_code': 1}
+  assert diff_files_stats(10, 11, 0, 1, 1) == \
+    {'correct': False, 'expected': 10, 'actual': 11,
+     'old': 0, 'new': 1, 'differences': 1,
+     'correct_ratio': 0.9523809523809523, 'correct_percent': 95.24, 'exit_code': 1}
+  assert diff_files_stats(10, 10, 1, 1, 1) == \
+    {'correct': False, 'expected': 10, 'actual': 10,
+     'old': 1, 'new': 1, 'differences': 2,
+     'correct_ratio': 0.9, 'correct_percent': 90.0, 'exit_code': 1}
+  assert diff_files_stats(0, 10, 0, 10, 0) == \
+    {'correct': False, 'expected': 0, 'actual': 10,
+     'old': 0, 'new': 10, 'differences': 10,
+     'correct_ratio': 0.0, 'correct_percent': 0.0, 'exit_code': 0}
 
 def diff_files_stats(*args):
-    result = util.diff_files_stats(*args)
-    # print(f'{repr(args)} -> {repr(result)}')
-    return result
+  result = util.diff_files_stats(*args)
+  # print(f'{repr(args)} -> {repr(result)}')
+  return result
