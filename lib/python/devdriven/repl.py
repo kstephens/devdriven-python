f270411188bcd336e1c844b44f3afc1ab5c298f4
Kurt Stephens <git@kurtstephens.com>
2023-11-05 16:32:20 -0600

    devdriven.repl: dont override vars.

--- lib/python/devdriven/repl.py
+++ lib/python/devdriven/repl.py
@@ -7,13 +7,14 @@ from pprint import pprint
 # https://stackoverflow.com/questions/50917938/enabling-console-features-with-code-interact
 # https://stackoverflow.com/questions/17248383/pretty-print-by-default-in-python-repl
 
-def start_repl(vars):
+def start_repl(bindings):
   readline_completer_save = readline.get_completer()
+  sys_displayhook_save = sys.displayhook
   try:
-    readline.set_completer(rlcompleter.Completer(vars).complete)
+    readline.set_completer(rlcompleter.Completer(bindings).complete)
     readline.parse_and_bind("tab: complete")
     sys.displayhook = lambda x: exec(['_=x; pprint(x)','pass'][x is None])
-    return code.InteractiveConsole(locals=vars).interact()
+    return code.InteractiveConsole(locals=bindings).interact()
   finally:
-    sys.displayhook = sys.__displayhook__
+    sys.displayhook = sys_displayhook_save
     readline.set_completer(readline_completer_save)
