fc4e878c2853b889fe0ca7e3785a219a75141dc0
Kurt Stephens <git@kurtstephens.com>
2023-11-17 11:23:54 -0600

    to_dict: PoC: extension-handlers.

--- lib/python/devdriven/to_dict.py
+++ lib/python/devdriven/to_dict.py
@@ -3,12 +3,16 @@ import subprocess
 import re
 import dataclasses
 import inspect
+from dataclasses import dataclass
 from typing import Any, Optional
 from datetime import datetime
 from pathlib import Path
 from devdriven.util import maybe_decode_bytes, datetime_iso8601
 
 class ToDict:
+  def __init__(self):
+    self.handlers = DEFAULT_HANDLERS
+
   def __call__(self, data: Any) -> Any:
     return self.walk(data)
 
@@ -67,7 +71,7 @@ class ToDict:
         'returncode': obj.returncode,
         'stdout': obj.stdout,
         'stderr': obj.stderr
-      })
+      } | vars(obj))
     if issubclass(obj_type, OSError):
       return self.walk({
         'class': obj_type.__name__,
@@ -82,8 +86,72 @@ class ToDict:
       'message': str(obj)
     } | vars(obj))
 
+  def match(self, obj):
+    obj_type = type(obj)
+    for handler in self.handlers:
+      result = handler.match(self, obj, obj_type)
+      if result is not UNMATCHED:
+        return result
+    return self.default_result(obj, obj_type)
+
+  def default_result(self, obj, obj_type):
+    return {'class': obj_type.__name__, 'repr': repr(obj)}
+
 def to_dict(data: Any) -> Any:
   return ToDict().walk(data)
 
 def dump_json(obj: Any, indent: Optional[int] = None) -> str:
   return json.dumps(to_dict(obj), indent=indent)
+
+UNMATCHED = object()
+
+#####################################
+
+@dataclass
+class Handler():
+  name: str = ''
+  matcher: callable = None
+  def match(self, walk, obj, obj_type):
+    return self.matcher(walk, obj, obj_type)
+
+  def parse_lines(self, lines):
+    def eat_empty():
+      while lines and not lines[0]:
+        lines.pop(0)
+    eat_empty()
+    if not lines:
+      return None
+    name = lines.pop(0)
+    body = [lines.pop(0)]
+    while lines and lines[0].startswith('  '):
+      body.append(lines.pop(0))
+    body.append('  return UNMATCHED')
+    return Handler(name, self.make_fun(['walk', 'obj', 'obj_type'], body))
+
+  def make_fun(self, params, body):
+    SEQUENCE[0] += 1
+    seq = SEQUENCE[0]
+    name = f'handler_fun_{seq}'
+    expr = f'def {name}({", ".join(params)}):\n  ' + '\n  '.join(body) + '\n'
+    print(expr)
+    bindings = globals() | {'UNMATCHED': UNMATCHED}
+    exec(expr, bindings)
+    return bindings[name]
+
+SEQUENCE = [0]
+
+def parse_handlers(defs):
+  handlers = []
+  lines = defs.splitlines()
+  while handler := lines and Handler().parse_lines(lines):
+    handlers.append(handler)
+  return handlers
+
+DEFAULT_HANDLERS = parse_handlers(
+'''
+
+'''
+)
+
+#import pprint
+#pprint.pprint(DEFAULT_HANDLERS)
