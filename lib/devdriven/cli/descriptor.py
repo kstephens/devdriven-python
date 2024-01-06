e912ded32bdadbb02bd522fdd76edc34fd0eb25b
Kurt Stephens <git@kurtstephens.com>
2024-01-06 13:51:08 -0600

    devdriven.cli.Descriptor: typing.

--- lib/devdriven/cli/descriptor.py
+++ lib/devdriven/cli/descriptor.py
@@ -1,12 +1,13 @@
+from __future__ import annotations
+from typing import Optional, List
 import re
 from dataclasses import dataclass
-from typing import List
 from devdriven.cli.options import Options
 from devdriven.util import set_from_match, unpad_lines
 from icecream import ic
 
 @dataclass
-class Descriptor():
+class Descriptor:
   klass: object
   name: str
   brief: str
@@ -22,10 +23,9 @@ class Descriptor():
   suffix_list: list
   data: object
 
-  def parse_docstring(self, docstr):
-    found_aliases = None
+  def parse_docstring(self, docstr: str) -> Descriptor:
+    found_aliases = False
     debug = False
-
     lines = unpad_lines(re.sub(r'\\\n', '', docstr).splitlines())
     comments = []
     while lines:
@@ -35,9 +35,9 @@ class Descriptor():
       m = None
       if m := re.match(r'^:(?P<name>[a-z_]+)[:=] *(?P<value>.*)', line):
         setattr(self, m.group('name'), m.group('value').strip())
-      elif m := not self.name and re.match(r'^(?P<name>[-a-z]+) +- +(?P<brief>.+)', line):
+      elif m := re.match(r'^(?P<name>[-a-z]+) +- +(?P<brief>.+)', line) if not self.name else None:
         set_from_match(self, m)
-      elif m := not found_aliases and re.match(r'(?i)^Alias(?:es)?: +(.+)', line):
+      elif m := re.match(r'(?i)^Alias(?:es)?: +(.+)', line) if not found_aliases else None:
         self.aliases.extend(re.split(r'\s*,\s*|\s+', m[1].strip()))
         found_aliases = True
       elif m := re.match(r'(?i)^(Arguments|Options|Examples):\s*$', line):
@@ -48,7 +48,7 @@ class Descriptor():
       elif m := re.match(r'^\$ (.+)', line):
         self.examples.append(Example(command=m[1], comments=comments))
         comments = []
-      elif m := self.options.parse_docstring(line):
+      elif self.options.parse_docstring(line):
         if debug:
           ic((self.name, self.options))
       else:
@@ -61,23 +61,23 @@ class Descriptor():
     self.trim_detail()
     return self
 
-  def preferred_suffix(self):
-    return self.suffix_list and self.suffix_list[0]
+  def preferred_suffix(self) -> Optional[str]:
+    return self.suffix_list[0] if self.suffix_list else None
 
   def get_opt_aliases(self, opt):
     return self.options.get_opt_aliases(opt)
 
-  def build_synopsis(self):
+  def build_synopsis(self) -> None:
     cmd = ['psv', self.name] + self.options.command_synopsis()
     self.synopsis = ' '.join(cmd)
 
-  def trim_detail(self):
+  def trim_detail(self) -> None:
     while self.detail and not self.detail[0]:
       self.detail.pop(0)
     while self.detail and not self.detail[-1]:
       self.detail.pop(-1)
 
 @dataclass
-class Example():
+class Example:
   command: str
   comments: List[str]
