a0f2cb124d7cbe4c5db9231b47e19e73cfbcba9b
Kurt Stephens <git@kurtstephens.com>
2024-01-06 14:02:13 -0600

    devdriven.mime: typing.

--- lib/devdriven/mime.py
+++ lib/devdriven/mime.py
@@ -1,15 +1,19 @@
+from __future__ import annotations
+from typing import Optional, List, Tuple
 import mimetypes
 import re
 from pathlib import Path
 
+StrOrNone = Optional[str]
+
 SUFFIX_RX = re.compile(r'(?:^|/)[^.]+(\.[^/]+)$')
-def short_and_long_suffix(path):
+def short_and_long_suffix(path: str) -> Tuple[str, str]:
   short_suffix = long_suffix = Path(path).suffix
   if m := re.match(SUFFIX_RX, str(path)):
     long_suffix = m[1]
   return short_suffix, long_suffix
 
-def content_type_for_suffixes(suffixes, default=(None, None)):
+def content_type_for_suffixes(suffixes: List[str], default=(None, None)) -> Tuple[StrOrNone, StrOrNone]:
   for suffix in suffixes:
     content_type, content_encoding = mimetypes.guess_type('anything' + suffix)
     if content_type:
