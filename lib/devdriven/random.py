2a3eb455d2912a276744dfb81b95a8753f9577dd
Kurt Stephens <git@kurtstephens.com>
2024-03-14 23:30:29 -0500

    devdriven.random: manage global seed.

---
+++ lib/devdriven/random.py
@@ -0,0 +1,26 @@
+from typing import Any, Optional
+from uuid import UUID, uuid4
+from random import Random, SystemRandom
+
+DEFAULT = SystemRandom()
+CURRENT = None
+SEED = None
+
+def random() -> Random:
+  return CURRENT or DEFAULT
+
+def set_seed(seed: Any) -> Random:
+  # pylint: disable-next=global-statement
+  global CURRENT, SEED
+  SEED = str(seed)
+  CURRENT = Random()
+  CURRENT.seed(SEED, version=2)
+  return CURRENT
+
+def get_seed() -> Optional[str]:
+  return SEED
+
+def uuid() -> str:
+  if CURRENT:
+    return str(UUID(bytes=random().randbytes(16), version=4))
+  return str(uuid4())
