5a4a51a22881a86ba59add75db9db2b397ba41a1
Kurt Stephens <git@kurtstephens.com>
2024-04-12 18:27:23 -0500

    mv resources_test.py resource_test.py.

--- lib/devdriven/resource_test.py
+++ lib/devdriven/resource_test.py
@@ -3,5 +3,5 @@ from devdriven.resource import Resources
 def test_resources():
   resources = Resources(search_paths=['.', 'lib', 'tests'])
   assert resources.find_all(['Makefile']) == ['Makefile']
-  assert resources.find(['devdriven/resources_test.py']) == 'lib/devdriven/resources_test.py'
+  assert resources.find(['devdriven/resource_test.py']) == 'lib/devdriven/resource_test.py'
   assert resources.find(['devdriven/data/actual.txt']) == 'tests/devdriven/data/actual.txt'
