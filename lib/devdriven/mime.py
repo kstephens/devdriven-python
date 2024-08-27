ed4cd7b833c5b1385b97b4c5fc248c2f71ae47ef
Kurt Stephens <git@kurtstephens.com>
2024-08-27 12:27:09 -0500

    devdriven.mime: fallback for markdown.

--- lib/devdriven/mime.py
+++ lib/devdriven/mime.py
@@ -14,7 +14,20 @@ def short_and_long_suffix(path: str) -> Tuple[str, str]:
 
 def content_type_for_suffixes(suffixes: List[str], default=(None, None)) -> Tuple[StrOrNone, StrOrNone]:
   for suffix in suffixes:
-    content_type, content_encoding = mimetypes.guess_type('anything' + suffix)
+    content_type, content_encoding = guess_type('anything' + suffix)
     if content_type:
       return content_type, content_encoding
   return default
+
+def guess_type(file: str) -> Tuple[Optional[str], Optional[str]]:
+  content_type, content_encoding = mimetypes.guess_type(file)
+  if not content_type:
+    content_type, content_encoding = MIMETYPES_MORE.get(Path(file).suffix, MIMETYPES_MORE[None])
+  return content_type, content_encoding
+
+
+MIMETYPES_MORE = {
+  '.md': ('text/markdown', None),
+  '.markdown': ('text/markdown', None),
+  None: (None, None),
+}
