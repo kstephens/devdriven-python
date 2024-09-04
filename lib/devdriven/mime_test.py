345affe1c517a1340a3a2a94b420325e65ec9fac
Kurt Stephens <git@kurtstephens.com>
2024-09-04 17:03:03 -0500

    lint.

--- lib/devdriven/mime_test.py
+++ lib/devdriven/mime_test.py
@@ -1,13 +1,10 @@
-import re
-import subprocess
-import pytest
 import devdriven.mime as sut
 
 def test_guess_type():
   fut = sut.guess_type
   assert fut("a") == (None, None)
   assert fut("a.c") == ('text/x-c', None)
-  assert fut("a.o") ==  ('application/octet-stream', None)
+  assert fut("a.o") == ('application/octet-stream', None)
   assert fut("a.md") == ('text/markdown', None)
   assert fut("a.markdown") == ('text/markdown', None)
 
@@ -15,7 +12,7 @@ def test_short_and_long_suffix():
   fut = sut.short_and_long_suffix
   assert fut("a") == ('', '')
   assert fut("a.c") == ('.c', '.c')
-  assert fut("a.tar.gz") ==  ('.gz', '.tar.gz')
+  assert fut("a.tar.gz") == ('.gz', '.tar.gz')
   assert fut("a.b/c.d.e") == ('.e', '.d.e')
 
 def test_content_type_for_suffixes():
