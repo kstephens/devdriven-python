23717eaf149a797423a82062e9c4e4463cd111fb
Kurt Stephens <git@kurtstephens.com>
2024-09-15 18:10:31 -0500

    build: fix requirements-files.

--- build/10-devdriven-python.sh
+++ build/10-devdriven-python.sh
@@ -2,4 +2,4 @@ bin-dir bin
 lib-dir lib
 test-dir lib
 js-dir lib/devdriven/resources/js lib/devdriven/resources/vendor/tablesort-*/src lib/devdriven/resources/vendor/tablesort-*/src/sorts
-requirements-dir .
+requirements-file *requirements.txt
