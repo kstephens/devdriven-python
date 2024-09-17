b9883e849c432632df19c4d14ca4a8db557f3304
Kurt Stephens <git@kurtstephens.com>
2024-09-16 23:23:23 -0500

    devdriven.glob: glob_to_regexp.

---
+++ lib/devdriven/glob_test.py
@@ -0,0 +1,22 @@
+import re
+import devdriven.glob as sut
+
+def test_glob_to_regex():
+  def fut(glob, path):
+    rx = sut.glob_to_regex(glob)
+    return re.search(rx, path) is not None
+  glob = 'a/b'
+  assert fut(glob, '') is False
+  assert fut(glob, 'a') is False
+  assert fut(glob, 'a/b') is True
+  assert fut(glob, 'a/bc') is False
+  glob = '*.c'
+  assert fut(glob, 'a.c') is True
+  assert fut(glob, 'b.b') is False
+  assert fut(glob, 'd/a.c') is False
+  assert fut(glob, '.c') is False
+  glob = '**/*.c'
+  assert fut(glob, 'a.c') is False
+  assert fut(glob, 'd/a.c') is True
+  assert fut(glob, 'd/e/a.c') is True
+  assert fut(glob, 'd/e/b') is False
