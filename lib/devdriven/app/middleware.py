9d1823146e0f17497fc62de6daaa8b41b207d8ef
Kurt Stephens <git@kurtstephens.com>
2024-11-25 08:46:41 -0600

    app.middleware: fixes and enhancements.

--- lib/devdriven/app/middleware.py
+++ lib/devdriven/app/middleware.py
@@ -2,30 +2,31 @@
 Application middleware combinators inspired Python WSGI and Ruby Rack.
 
 An "App" is anything callable with a single dict argument:
-It receives a "Request": typically a Dict of input: headers, body and customer values passed along an "application stack".
+It receives a "Request": typically a Dict of input:
+headers, body and custom values passed along an "application stack".
 It returns a "Response": Tuple of HTTP status code, headers and a body (sequence of response chunks).
-Both applications and middleware follow the same protocol.
+Applications and middleware follow the same protocol.
 Combinators create new Apps by wrapping others.
 
 Input combinators follow this pattern:
 
 def compose_input_handler(app: App) -> App:
-    def input_handler(req: Req) -> Res:
-        do_something_with_input(req)
-        return app(req)
+  def input_handler(req: Req) -> Res:
+    do_something_with_input(req)
+    return app(req)
+  return input_handler
 
 Output combinators follow this pattern:
 
 def compose_output_handle(app: App):
-    def output_handler(req: Req) -> Res:
-            status, headers, body = app(req)
-            # alter status, header, body in some manner.
-            return status, headers, body
-        return status, headers,
-
+  def output_handler(req: Req) -> Res:
+    status, headers, body = app(req)
+    # alter status, header, body in some manner.
+    return status, headers, body
+  return output_handler
 """
 
-from typing import Any, Dict, Tuple, Iterable, Callable, Optional
+from typing import Any, MutableMapping, Tuple, Iterable, Callable, Optional
 import json
 import re
 import sys
@@ -34,59 +35,73 @@ from pprint import pprint
 from http.client import responses as response_names
 
 Status = int
-Headers = Dict[str, Any]
+Headers = MutableMapping[str, Any]
 Body = Iterable
 Res = Tuple[Status, Headers, Body]
-Req = Dict[str, Any]
+Req = MutableMapping[str, Any]
 App = Callable[[Req], Res]
 
 
 ### Developer Affordance
 
+
 def is_success(status: Status) -> bool:
     return status in range(200, 300)
 
+
 #### Exception Handling
+
 
 def capture_exception(app: App, status=500, cls=Exception, with_traceback=False) -> App:
     "Captures any exception and creates a text/plain error document."
-    def capturing_exception(req: Req) -> Res:
+
+    def _capture_exception(req: Req) -> Res:
         try:
             return app(req)
-        # pylint: disable-next=broad-exception-caught
+        # pylint: disable-next=broad-exception-caught,broad-except
         except cls as exc:
-            req['captured.exception'] = exc
+            req["captured.exception"] = exc
             body = [f"ERROR: {exc}"]
             if with_traceback:
-                tb = exc.__traceback__
-                tbs = traceback.extract_tb(tb)
-                lines = traceback.format_list(tbs)
+                trb = exc.__traceback__
+                trb_extracted = traceback.extract_tb(trb)
+                lines = traceback.format_list(trb_extracted)
                 body.extend(lines)
             return status, {"Content-Type": "text/plain"}, body
-    return capturing_exception
+
+    return _capture_exception
+
 
 #### Tracing
 
+
 def trace(app: App, ident="", stream=sys.stderr) -> App:
     "Traces requests and responses."
+
     def indent(msg):
         stream.write(f"{'  ' * TRACE_INDENT[0]}{msg}")
+
     def log(msg):
         indent(f" #{msg} {ident}\n")
-    def pp(data):
+
+    def prnt(data):
         indent("")
         pprint(data, stream=stream)
-    def tracing(req: Req) -> Res:
+
+    def _trace(req: Req) -> Res:
         log(">>>")
         TRACE_INDENT[0] += 1
-        pp(req)
+        prnt(req)
         result = app(req)
         log("...")
-        pp(result)
+        prnt(result)
         TRACE_INDENT[0] -= 1
         log("<<<")
         return result
-    return tracing
+
+    return _trace
+
+
 TRACE_INDENT = [0]
 
 ## Reading Input, Writing Output
@@ -96,32 +111,41 @@ Data = Any
 
 ### Reading input
 
+
 def read_input(app: App, read: Optional[Callable[[Data], Content]] = None) -> App:
     "Reads body.stream"
     if not read:
+        # pylint: disable-next=unnecessary-lambda-assignment
         read = lambda stream: stream.read()
-    def reader(req: Req) -> Res:
+
+    def _read_input(req: Req) -> Res:
         assert read is not None
         req["input.content"] = read(req["input.stream"])
         return app(req)
-    return reader
+
+    return _read_input
+
 
 ### Writing Output
 
+
 def write_output(app: App) -> App:
     "Reads body.stream"
-    def writer(req: Req) -> Res:
+
+    def _write_output(req: Req) -> Res:
         status, headers, body = app(req)
         stream = req["output.stream"]
         for item in body:
             stream.write(item)
         return status, headers, body
-    return writer
+
+    return _write_output
+
 
 def http_response(app: App) -> App:
     "Generates an HTTP response, emulating a web server."
 
-    def http(req: Req) -> Res:
+    def _http_response(req: Req) -> Res:
         status, headers, body = app(req)
         out = [f"HTTP/1.1 {status} {response_names.get(status, 'Unknown')}\n"]
         for k, v in headers.items():
@@ -130,21 +154,29 @@ def http_response(app: App) -> App:
         out.extend(body)
         return status, headers, out
 
-    return http
+    return _http_response
+
 
 def capitalize_headers(app: App) -> App:
     "Capitalizes-All-Headers."
-    def capitalizing_headers(req: Req) -> Res:
+
+    def _capitalize_headers(req: Req) -> Res:
         status, headers, body = app(req)
         headers = {header_key(k): v for k, v in headers.items()}
         return status, headers, body
-    return capitalizing_headers
+
+    return _capitalize_headers
+
 
 def header_key(k: str) -> str:
     "Returns a 'A-Key-Word' for 'a-key-word"
     return re.sub(WORD_RX, lambda m: capitalize(str(m[1])), k.replace("_", "-"))
+
+
 def capitalize(k: str):
     return k[0].upper() + k[1:]
+
+
 WORD_RX = re.compile(r"\b([a-z]+)\b")
 
 
@@ -160,19 +192,21 @@ def decode_content(app: App, decoder: Decoder, content_types=None, strict=False)
     If strict and Content-Type is not expected, return 400.
     """
 
-    def decoding_content(req: Req) -> Res:
+    def _decode_content(req: Req) -> Res:
         req["input.data"] = decoder(req["input.content"])
         content_type = req.get("Content-Type")
         if strict and content_types and content_type not in content_types:
             msg = f"Unexpected Content-Type {content_type!r} : expected: {content_types!r} : "
-            return 400, {"Content-Type": 'text/plain'}, (msg,)
+            return 400, {"Content-Type": "text/plain"}, (msg,)
         return app(req)
-    return decoding_content
+
+    return _decode_content
 
 
 def encode_content(app: App, encoder: Encoder, content_type="text/plain") -> App:
     "Encodes body with encoder.  Sets Content-Type."
-    def encoding_content(req: Req) -> Res:
+
+    def _encode_content(req: Req) -> Res:
         status, headers, body = app(req)
         content = "".join(map(encoder, body))
         headers |= {
@@ -180,60 +214,117 @@ def encode_content(app: App, encoder: Encoder, content_type="text/plain") -> App
             "Content-Length": len(content),
         }
         return status, headers, [content]
-    return encoding_content
+
+    return _encode_content
 
 
 ## Decode JSON, Encode JSON
 
+
 def decode_json(app: App, **kwargs) -> App:
     "Decodes JSON content."
-    def decoding_json(content: Content) -> Any:
+
+    def _decode_json(content: Content) -> Any:
         return json.loads(content, **kwargs)
-    return decode_content(app, decoding_json, content_types={'application/json', 'text/plain'}, strict=True)
+
+    return decode_content(
+        app, _decode_json, content_types={"application/json", "text/plain"}, strict=True
+    )
 
 
 def encode_json(app: App, **kwargs) -> App:
     "Encodes data as JSON."
-    def encoding_json(data: Data) -> Content:
+
+    def _encode_json(data: Data) -> Content:
         return json.dumps(data, **kwargs) + "\n"
-    return encode_content(app, encoding_json, content_type='application/json')
+
+    return encode_content(app, _encode_json, content_type="application/json")
+
 
 ## Header Management
 
-def content_length(app: App, **kwargs) -> App:
-    def compute_content_length(req: Req) -> Res:
+
+def content_length(app: App) -> App:
+    def _content_length(req: Req) -> Res:
         status, headers, body = app(req)
-        headers['Content-Length'] = sum(map(len, body))
+        headers["Content-Length"] = sum(map(len, body))
         return status, headers, body
-    return compute_content_length
+
+    return _content_length
+
 
 ## Injection
 
-def default(app: App, defaults: Req) -> App:
+
+def default_req(app: App, defaults: Req) -> App:
     "Defaults any req values that are not defined."
 
-    def defaulter(req: Req):
-        for k, v in defaults.items():
-            if k not in req:
-                req[k] = v
-        return app(req)
+    def _default_req(req: Req):
+        return app(default_dict(req, defaults))
 
-    return defaulter
+    return _default_req
 
 
-def override(app: App, overrides: Req) -> App:
+def override_req(app: App, overrides: Req) -> App:
     "Overrides req values."
 
-    def overrider(req: Req):
+    def _override_req(req: Req):
         req.update(overrides)
         return app(req)
 
-    return overrider
+    return _override_req
+
+
+ResOptional = Tuple[Optional[Status], Optional[Headers], Optional[Body]]
+AppOptional = Callable[[Req], ResOptional]
+
+
+def default_res(app: AppOptional, defaults: ResOptional) -> App:
+    "Defaults any values that are not defined."
+
+    def _default_res(req: Req):
+        status_, headers_, body_ = defaults
+        status, headers, body = app(req)
+        if status is None and status_ is not None:
+            status = status_
+        if headers_:
+            default_dict(headers, headers_)
+        if body is None and body_ is not None:
+            body = body_
+        return status, default_dict(headers, defaults), body
+
+    return _default_res
+
+
+def override_res(app: App, overrides: ResOptional) -> App:
+    "Overrides response status, headers, body."
+
+    def _override_res(req: Req):
+        status_, headers_, body_ = overrides
+        status, headers, body = app(req)
+        if status_ is not None:
+            status = status_
+        if headers_:
+            headers.update(headers_)
+        if body_ is not None:
+            body = body_
+        return status, headers, body
+
+    return _override_res
+
+
+def default_dict(d, defaults):
+    for k, v in defaults.items():
+        if k not in d:
+            d[k] = v
+    return d
+
 
 ## Composition
 
 
 ### Adapters
+
 
 def read_wsgi(app: App) -> App:
     return read_input(app, lambda req: req["wsgi.input"].read())
