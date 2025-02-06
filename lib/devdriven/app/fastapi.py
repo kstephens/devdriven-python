87f51e938eef880d33b6d8ac419da59eda3e5791
Kurt Stephens <git@kurtstephens.com>
2025-02-05 18:59:30 -0600

    fastapi support.

--- lib/devdriven/app/fastapi.py
+++ lib/devdriven/app/fastapi.py
@@ -1,8 +1,10 @@
-'''
+"""
 fastapi helpers.
-'''
-import fastapi.encoders as encoders
+"""
+
+import fastapi.encoders
 from fastapi.encoders import jsonable_encoder as jsonable_encoder_orig
+
 
 def enable_as_dict_encoder():
     orig = jsonable_encoder_orig
@@ -14,7 +16,8 @@ def enable_as_dict_encoder():
             args[0] = encode()
         return orig(*args, **kwargs)
 
-    encoders.jsonable_encoder = as_dict_encoder
+    fastapi.encoders.jsonable_encoder = as_dict_encoder
+
 
 def disable_as_dict_encoder():
-    encoders.jsonable_encoder = jsonable_encoder_orig
+    fastapi.encoders.jsonable_encoder = jsonable_encoder_orig
